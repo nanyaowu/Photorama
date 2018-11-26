8de9cc4785ace1b091c58fbe4697e0ea43e9af12
Nan-Yao Wu <nanyao.h.wu@gmail.com>
2018-11-26 12:21:00 +0800

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by freakytune on 2018/11/26.
+//  Copyright © 2018 freakytune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
